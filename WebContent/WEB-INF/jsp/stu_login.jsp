0857b7c2a4806c0e9fedf303c15e673b9cb5efd9
1501091 <1501091@PC63302.asost.local>
2017-06-16 12:58:03 +0900

    よろしくお願いします！

--- WebContent/WEB-INF/jsp/stu_login.jsp
+++ WebContent/WEB-INF/jsp/stu_login.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ include file="/WEB-INF/jsp/common.jsp"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -14,12 +15,12 @@
 	<table>
 		<tr>
 			<td>ID</td>
-			<td><input type="text" name="user_id" size="10"></td>
+			<td><input type="text" name="stu_id" size="15"></td>
 		</tr>
 
 		<tr>
 			<td>Password</td>
-			<td><input type="password" name="password" size="10"></td>
+			<td><input type="password" name="stu_password" size="15"></td>
 		</tr>
 
 	</table>
--- src/model/Student.java
+++ src/model/Student.java
@@ -8,29 +8,38 @@ public class Student implements Serializable {
 	private String stu_name;
 	private String stu_password;
 	private String clas;
+
+	public Student(){}
+
 	public int getStu_id() {
 		return stu_id;
 	}
+
 	public void setStu_id(int stu_id) {
 		this.stu_id = stu_id;
 	}
+
 	public String getStu_name() {
 		return stu_name;
 	}
+
 	public void setStu_name(String stu_name) {
 		this.stu_name = stu_name;
 	}
+
 	public String getStu_password() {
 		return stu_password;
 	}
+
 	public void setStu_password(String stu_password) {
 		this.stu_password = stu_password;
 	}
+
 	public String getClas() {
 		return clas;
 	}
+
 	public void setClas(String clas) {
 		this.clas = clas;
 	}
-
 }
